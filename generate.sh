c015357653e5fff8aa36366d83db7073f3651e48
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2021-04-30 16:19:53 +0100

    Script to generate html file

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+HELLO_FILE="${SCRIPT_DIR}/public-html/hello.html"
+
+ if [ -z "$1" ]; then echo "No person name provided"; exit 1; fi
+PERSON_NAME=$1
+
+echo "<html><body><h1>Hello ${PERSON_NAME}!<h1></body></html>" | tee $HELLO_FILE 2>/dev/null
+
+if [[ ! -f "${HELLO_FILE}" ]]; then
+  echo "ERROR - Failed to generate html file"
+  exit 1
+fi
